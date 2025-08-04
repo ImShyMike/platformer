bb7825184ea0648073f60db5caae3126ad945133
ShyMike <122023566+ImShyMike@users.noreply.github.com>
2025-08-04 12:58:36 +0100

    moving platform

---
+++ platformer/animatable_body_2d.gd
@@ -0,0 +1,14 @@
+extends AnimatableBody2D
+@export var move_distance = Vector2(200, 0)
+@export var move_duration = 2.0
+var start_position: Vector2
+var tween: Tween
+
+func _ready():
+	start_position = global_position
+	sync_to_physics = true
+	
+	tween = create_tween()
+	tween.set_loops()
+	tween.tween_property(self, "global_position", start_position + move_distance, move_duration)
+	tween.tween_property(self, "global_position", start_position, move_duration)
